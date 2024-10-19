e426e84b25572e74e0e436c839e39ee1456722e7
Tommaso <tommaso.divito2@gmail.com>
2024-10-19 17:27:00 +0200

    More distribution analysis

--- dataunderstander.py
+++ dataunderstander.py
@@ -24,16 +24,21 @@ class DataUnderstander:
         for i, k in enumerate(correlations.keys()):
             correlations[k].loc[:, "correlation_type"] = k
 
-        fig, axes = plt.subplots(1, 3, figsize=(18, 6))  # Create 3 subplots for "kendall", "pearson", and "spearman"
-
-        for i, (corr_type, corr_matrix) in enumerate(correlations.items()):
+        # Loop through each correlation type and create separate plots
+        for corr_type, corr_matrix in correlations.items():
             corr_matrix = corr_matrix.drop(columns=["correlation_type"])
 
-            sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5, ax=axes[i])
-            axes[i].set_title(f'{corr_type.capitalize()} Correlation')
+            # Create a new figure for each correlation type
+            plt.figure(figsize=(12, 12))
 
-        plt.tight_layout()
-        plt.show()
+            # Plot the heatmap
+            sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
+
+            # Set the title for the plot
+            plt.title(f'{corr_type.capitalize()} Correlation')
+
+            # Display the plot
+            plt.show()
     
     def normalize(self):
         numeric_columns = ['points', 'length', 'climb_total', 'profile', 'startlist_quality', 'cyclist_age', 'delta', 'birth_year', 'weight', 'height']
